520838654160c8ed0273b1592ecd00ca22f59f6a
todd densmore <469399+tdensmore@users.noreply.github.com>
2021-05-14 02:14:55 -0400

    Updated harbor version

--- 2-download.sh
+++ 2-download.sh
@@ -1,16 +1,16 @@
-VERSION="harbor-online-installer-v2.0.1.tgz"
+VERSION="v2.1.5"
 
 ## download the installer
-curl -LO https://github.com/goharbor/harbor/releases/download/v2.0.1/$VERSION
+curl -LO https://github.com/goharbor/harbor/releases/download/${VERSION}/harbor-online-installer-${VERSION}.tgz
 
 ## download the asc file
-curl -LO https://github.com/goharbor/harbor/releases/download/v2.0.1/$VERSION.asc
+curl -LO https://github.com/goharbor/harbor/releases/download/${VERSION}/harbor-online-installer-${VERSION}.tgz.asc
 
 ## download Harbor key
 gpg --keyserver hkps://keyserver.ubuntu.com --receive-keys 644FF454C0B4115C
 
 ## verify the key
-gpg -v --keyserver hkps://keyserver.ubuntu.com --verify $VERSION.asc
+gpg -v --keyserver hkps://keyserver.ubuntu.com --verify harbor-online-installer-${VERSION}.tgz.asc
 
 ## unpack the installer
 tar xvf $VERSION 
