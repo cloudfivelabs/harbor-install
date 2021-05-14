59b9f8ef552f766b92e32bdf9a0d78e62c61510c
todd densmore <469399+tdensmore@users.noreply.github.com>
2021-05-14 02:09:53 -0400

    Updated docker-compose

--- 1-docker.sh
+++ 1-docker.sh
@@ -1,3 +1,4 @@
+DOCKER_COMPOSE="1.29.1"
 
 ## remove old versions
 sudo apt-get remove docker docker-engine docker.io containerd runc
@@ -28,17 +29,22 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
+# add user to docker group
+sudo usermod -aG docker $(whoami)
+
 ## verify
 sudo docker run hello-world
 
-sudo usermod -aG docker $(whoami)
-
 ## install docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 ## make the binary executable
 sudo chmod +x /usr/local/bin/docker-compose
 
-## install command completion
-sudo curl -L https://raw.githubusercontent.com/docker/compose/1.26.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+# link it
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
+## install command completion
+sudo curl -L https://raw.githubusercontent.com/docker/compose/${DOCKER_COMPOSE}/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
+docker-compose --version
